ff6d06b5c4e5206524676e8b8d5ac315a3c35ddc
Rohit Jain <rohitjain@TMDCIN095.local>
2024-08-22 13:28:53 +0530

    change reveune trend

--- apis/revenuetrend.sql
+++ apis/revenuetrend.sql
@@ -1,10 +1,17 @@
+WITH last_invoice AS (
+    SELECT 
+        MAX(invoice_date) AS last_invoice_date
+    FROM 
+        sales
+)
 SELECT 
     date_trunc('day', invoice_date) AS day,
     SUM(total_revenue) AS revenue
 FROM 
-    sales
+    sales, last_invoice
 WHERE 
-    invoice_date >= CURRENT_DATE - INTERVAL '7 days'
+    invoice_date >= last_invoice.last_invoice_date - INTERVAL '7 days'
+    AND invoice_date <= last_invoice.last_invoice_date
 GROUP BY 
     day
 ORDER BY 
