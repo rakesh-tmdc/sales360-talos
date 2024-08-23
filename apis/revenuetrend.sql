54cd22345d04f6f0cc74fff710dcee892aec9c4f
Rohit Jain <rohitjain@TMDCIN095.local>
2024-08-23 17:50:23 +0530

    revert back

--- apis/revenuetrend.sql
+++ apis/revenuetrend.sql
@@ -7,7 +7,7 @@ WITH last_invoice AS (
 )
 SELECT 
     date_trunc('day', invoice_date) AS day,
-    total_revenue AS revenue
+    SUM(total_revenue) AS revenue
 FROM 
     sales_cache, last_invoice
 WHERE 
