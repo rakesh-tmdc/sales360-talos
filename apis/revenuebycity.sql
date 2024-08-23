2b415df231627b36d4c69e97a36978297fd40212
Rohit Jain <rohitjain@TMDCIN095.local>
2024-08-23 15:34:17 +0530

    change revune by city

--- apis/revenuebycity.sql
+++ apis/revenuebycity.sql
@@ -1,1 +1,2 @@
-select sales.total_revenue, account.city from sales left join account on sales.__joinField = account.__joinField
+-- select sales.total_revenue, account.city from sales left join account on sales.__joinField = account.__joinField
+select measure(total_revenue) as total_revenue , account.city from sales left join account on sales.__joinField = account.__joinField group by 2;
