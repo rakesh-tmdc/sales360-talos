230763a2e9f3beaa61a55acafc05c1239c998014
Rohit Jain <rohitjain@TMDCIN095.local>
2024-09-02 17:34:39 +0530

    removed hugging face api

--- apis/allmetrics.sql
+++ apis/allmetrics.sql
@@ -52,3 +52,17 @@ FROM
 
 {% endcache %}
 {% endreq %}
+
+{% set query_result = {
+    "total_revenue_last_invoice_date": 20136,
+    "percentage_change": null,
+    "trend_last_7_days": [117816, 43284, 53514, 75294, 85902, 7692, 20136]
+} %}
+{% set var =  {
+        "headers": { "Authorization" : "Bearer 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" },
+        "body" : { "inputs" : query_result }
+    } 
+%}
+
+
+SELECT {{ var |rest_api(url='https://api-inference.huggingface.co/models/facebook/bart-large-cnn', method = 'POST') }}
